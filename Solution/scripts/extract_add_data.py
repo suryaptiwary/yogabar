03322efd053cbcbd372ca168e2258d6a9338eee0
suryaptiwary <surya.ptiwary.min14@itbhu.ac.in>
2020-09-20 19:17:10 +0530

    added comments/edits

--- Solution/scripts/extract_add_data.py
+++ Solution/scripts/extract_add_data.py
@@ -7,10 +7,11 @@ from dateutil import parser as dateparser
 from time import sleep
 from extract_review_text import link
 
+total_reviews = 882       #number of reviews (assumed from product page)
 cookie = {}
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
 
-def GetAdditionalData(link):
+def GetAdditionalData(link):  #extracts rating, review header, review posted date and author name and returns them as a dictionary
   page = requests.get(link, headers = header, cookies = cookie)
   page_response = page.text
 
@@ -27,10 +28,12 @@ def GetAdditionalData(link):
   reviews_list = []
 
   if not reviews:
-      raise ValueError('Unable to find reviews in page')
+      raise ValueError('Unable to find reviews')
 
-    # Parsing individual reviews
-  for review in reviews:
+      
+  for review in reviews:  
+      
+      #parsing individual reviews
       XPATH_RATING  = './/i[@data-hook="review-star-rating"]//text()'
       XPATH_REVIEW_HEADER = './/a[@data-hook="review-title"]//text()'
       XPATH_REVIEW_POSTED_DATE = './/span[@data-hook="review-date"]//text()'
@@ -41,7 +44,7 @@ def GetAdditionalData(link):
       raw_review_header = review.xpath(XPATH_REVIEW_HEADER)
       raw_review_posted_date = review.xpath(XPATH_REVIEW_POSTED_DATE)
     
-      # Cleaning data
+      #cleaning parsed data
       author = ' '.join(' '.join(raw_review_author).split())
       review_rating = ''.join(raw_review_rating).replace('out of 5 stars', '')
       review_header = ' '.join(' '.join(raw_review_header).split())
@@ -52,19 +55,18 @@ def GetAdditionalData(link):
                 'review_header':review_header,
                 'review_rating':review_rating,
                 'review_author':author
-
               }
       reviews_list.append(review_dict)
   
   return reviews_list
 
-extracted_data = []
+extracted_data = []   #stores list of all reviews (from all review pages) stored as dictionary
 
-for k in range(88):
+for k in range(total_reviews//10): #extracts additional review data from all pages, assuming each page contains 10 reviews
   data = GetAdditionalData(link+'&pageNumber='+str(k))
   for review in data:
         extracted_data.append(review)
  
-df = pd.DataFrame(extracted_data)
-df.to_csv('add_review_data.csv',index=False)
+df = pd.DataFrame(extracted_data) #converts list of dictionaries to dataframe
+df.to_csv('add_review_data.csv',index=False) #converts dataframe to csv
 
--- Solution/scripts/extract_review_text.py
+++ Solution/scripts/extract_review_text.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import requests
 import json, re
-
 from bs4 import BeautifulSoup
-#from dateutil import parser as dateparser
-#from lxml import html 
 
-with open('D:\Yogabar\Solution\product_data.json') as file:
+with open("D:/Yogabar/Solution/data/assignment_data.json") as file:
     product_data = json.load(file)
 
-reviews = []              #stores reviews of all review pages (1-2-3...)
+total_reviews = 882       #number of reviews (assumed from product page)
+reviews = []              #stores individual review text of all review pages (1-2-3...)
 url = product_data["url"] #the given url of the product reviews page (https://www.amazon.in/Yogabar-Wholegrain-Breakfast-Muesli-Fruits/product-reviews/B07M6KZQCN)
 asin_number = url[-10:]   #ASIN = B07M6KZQCN, extracts last 10 characters of "url"
 cookie = {}                 
@@ -34,16 +32,16 @@ def GetAllReviewsLink(asin): #function which returns "see all reviews" link taki
     else:
         return "Not Found"
 
-link = GetAllReviewsLink(asin_number) #
+link = GetAllReviewsLink(asin_number) #https://www.amazon.in/Yogabar-Wholegrain-Breakfast-Muesli-Fruits/product-reviews/B07M6KZQCN/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews
 
-for k in range(89):   #extract reviews from all pages, assuming each page contains 10 reviews
-    response = GetReviewContent(link +'&pageNumber='+str(k))
+for k in range(total_reviews//10):   #extracts review text from all pages, assuming each page contains 10 reviews
+    response = GetReviewContent(link +'&pageNumber='+str(k)) #adding '&pageNumber=k' to 'link' to parse each review page
     soup = BeautifulSoup(response.content)
     for i in soup.findAll("span",{'data-hook':"review-body"}):
         reviews.append(i.text)
      
-rev_dict = {'reviews':reviews}                  #converting the reviews list into a dictionary
-review_data = pd.DataFrame.from_dict(rev_dict)  #converting this dictionary into a dataframe
-review_data.to_csv('review_text_data.csv',index=False)  #converting the dataframe to csv
+rev_dict = {'reviews':reviews}                  #converts the reviews list into a dictionary
+review_data = pd.DataFrame.from_dict(rev_dict)  #converts this dictionary into a dataframe
+review_data.to_csv('review_text_data.csv',index=False)  #converts the dataframe to csv
 
 
